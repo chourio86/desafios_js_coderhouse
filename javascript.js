286d7382ffe8b099051f15214d20dddc0aa0bcdf
chourio86 <alex8612.06jr@gmail.com>
2023-04-20 13:04:51 -0500

    Primer desafio js Coder

---
+++ javascript.js
@@ -0,0 +1,38 @@
+    
+    ///ACTIVIDAD 1
+
+    /* let n = parseInt(prompt("Ingrese un numero"));
+
+     for( let i=1; i <=5; i++){
+        let resultado = n + i;
+        alert(n + "+" + i + "=" + resultado);
+     }*/
+
+
+
+
+     ///ACTIVIDAD 2
+/* 
+     let dato = prompt('INGRESA UN DATO');
+
+     while ( dato != "ESC"){
+            alert('USTED INGRESO: ' + dato);
+ 
+        let dato2 = prompt('INGRESA OTRO DATO');
+            alert ('USTED INGRESO ' + dato2+ ' SI NO DESEA CONTINUAR ESCRIBE ESC');
+
+        dato = prompt('DESEA CONTINUAR?');
+
+  } */
+
+
+
+        ///ACTIVIDAD 3
+
+       /* let numero = prompt('INGRESA UN NUMERO')
+
+        for (let i=1; i<=numero; i++){
+            console.log('SE REPETIRA ' + numero + ' VECES ESTE MENSAJE: "Hola Coder"');
+            
+            
+        }*/
